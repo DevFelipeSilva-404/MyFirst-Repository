c2e5bae3928b3e5219218cf19aecfc5424d7860b
DevFelipeSilva-404 <DevFelipeSilvaA@gmail.com>
2025-04-03 17:33:35 +0000

    adiciona os scripts  `lint:check` e `lint:fix`

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
-function Home(){
-    
-    return `<h2>Esse é meu primeiro Deploy :3
-         </h2>` 
+function Home() {
+  return `<h2>Esse é meu primeiro Deploy :3
+         </h2>`;
 }
 
 export default Home;
